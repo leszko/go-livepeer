8f942f5269bab683d92dcb33eebf35f1896203e5
Rafal Leszko <rafal@livepeer.org>
2022-03-25 14:08:06 +0100

    discovery: Move discovery to a separate OrchInfoCache struct

--- discovery/discovery.go
+++ discovery/discovery.go
@@ -7,6 +7,7 @@ import (
 	"math"
 	"math/rand"
 	"net/url"
+	"sync"
 	"time"
 
 	"github.com/livepeer/go-livepeer/clog"
@@ -25,89 +26,28 @@ var maxGetOrchestratorCutoffTimeout = 6 * time.Second
 var serverGetOrchInfo = server.GetOrchestratorInfo
 
 type orchestratorPool struct {
-	infos []common.OrchestratorLocalInfo
-	pred  func(info *net.OrchestratorInfo) bool
-	bcast common.Broadcaster
+	infos         []common.OrchestratorLocalInfo
+	pred          func(info *net.OrchestratorInfo) bool
+	bcast         common.Broadcaster
+	orchInfoCache OrchInfoCache
 }
 
-func NewOrchestratorPool(bcast common.Broadcaster, uris []*url.URL, score float32) *orchestratorPool {
-	if len(uris) <= 0 {
-		// Should we return here?
-		glog.Error("Orchestrator pool does not have any URIs")
-	}
-	infos := make([]common.OrchestratorLocalInfo, 0, len(uris))
-	for _, uri := range uris {
-		infos = append(infos, common.OrchestratorLocalInfo{URL: uri, Score: score})
-	}
-
-	return &orchestratorPool{infos: infos, bcast: bcast}
+type OrchInfoCache struct {
+	orchInfosCached []*net.OrchestratorInfo
+	mu              sync.RWMutex
 }
 
-func NewOrchestratorPoolWithPred(bcast common.Broadcaster, addresses []*url.URL,
-	pred func(*net.OrchestratorInfo) bool, score float32) *orchestratorPool {
-
-	pool := NewOrchestratorPool(bcast, addresses, score)
-	pool.pred = pred
-	return pool
-}
-
-func (o *orchestratorPool) GetInfos() []common.OrchestratorLocalInfo {
-	return o.infos
-}
-
-func (o *orchestratorPool) GetInfo(uri string) common.OrchestratorLocalInfo {
-	var res common.OrchestratorLocalInfo
-	for _, info := range o.infos {
-		if info.URL.String() == uri {
-			res = info
-			break
-		}
-	}
-	return res
-}
-
-func (o *orchestratorPool) GetOrchestrators(ctx context.Context, numOrchestrators int, suspender common.Suspender, caps common.CapabilityComparator,
-	scorePred common.ScorePred) ([]*net.OrchestratorInfo, error) {
-
-	linfos := make([]common.OrchestratorLocalInfo, 0, len(o.infos))
-	for _, info := range o.infos {
+func (oic *OrchInfoCache) refresh(ctx context.Context, numOrchestrators int, suspender common.Suspender, localInfos []common.OrchestratorLocalInfo, scorePred common.ScorePred, bcast common.Broadcaster) {
+	linfos := make([]common.OrchestratorLocalInfo, 0, len(localInfos))
+	for _, info := range localInfos {
 		if scorePred(info.Score) {
 			linfos = append(linfos, info)
 		}
 	}
 
-	numAvailableOrchs := len(linfos)
-	numOrchestrators = int(math.Min(float64(numAvailableOrchs), float64(numOrchestrators)))
-
-	// The following allows us to avoid capability check for jobs that only
-	// depend on "legacy" features, since older orchestrators support these
-	// features without capability discovery. This enables interop between
-	// older orchestrators and newer orchestrators as long as the job only
-	// requires the legacy feature set.
-	//
-	// When / if it's justified to completely break interop with older
-	// orchestrators, then we can probably remove this check and work with
-	// the assumption that all orchestrators support capability discovery.
-	legacyCapsOnly := caps.LegacyOnly()
-
-	isCompatible := func(info *net.OrchestratorInfo) bool {
-		if o.pred != nil && !o.pred(info) {
-			return false
-		}
-		// Legacy features already have support on the orchestrator.
-		// Capabilities can be omitted in this case for older orchestrators.
-		// Otherwise, capabilities are required to be present.
-		if info.Capabilities == nil {
-			if legacyCapsOnly {
-				return true
-			}
-			return false
-		}
-		return caps.CompatibleWith(info.Capabilities)
-	}
 	getOrchInfo := func(ctx context.Context, uri *url.URL, infoCh chan *net.OrchestratorInfo, errCh chan error) {
-		info, err := serverGetOrchInfo(ctx, o.bcast, uri)
-		if err == nil && isCompatible(info) {
+		info, err := serverGetOrchInfo(ctx, bcast, uri)
+		if err == nil {
 			infoCh <- info
 			return
 		}
@@ -120,12 +60,16 @@ func (o *orchestratorPool) GetOrchestrators(ctx context.Context, numOrchestrator
 		errCh <- err
 	}
 
+	numAvailableOrchs := len(linfos)
+	numOrchestrators = int(math.Min(float64(numAvailableOrchs), float64(numOrchestrators)))
+
 	// Shuffle into new slice to avoid mutating underlying data
 	uris := make([]*url.URL, numAvailableOrchs)
 	for i, j := range rand.Perm(numAvailableOrchs) {
 		uris[i] = linfos[j].URL
 	}
 
+	// TODO: Implement the batching parallel requests, to have max. 100 parallel HTTP requests
 	var infos []*net.OrchestratorInfo
 	suspendedInfos := newSuspensionQueue()
 	timedOut := false
@@ -171,17 +115,118 @@ func (o *orchestratorPool) GetOrchestrators(ctx context.Context, numOrchestrator
 	}
 	cancel()
 
-	if len(infos) < numOrchestrators {
-		diff := numOrchestrators - len(infos)
-		for i := 0; i < diff && suspendedInfos.Len() > 0; i++ {
-			info := heap.Pop(suspendedInfos).(*suspension).orch
+	oic.mu.Lock()
+	oic.orchInfosCached = infos
+	oic.mu.Unlock()
+
+}
+
+func (oic OrchInfoCache) orchInfos() []*net.OrchestratorInfo {
+	oic.mu.RLock()
+	defer oic.mu.RUnlock()
+	return oic.orchInfosCached
+}
+
+func NewOrchestratorPool(bcast common.Broadcaster, uris []*url.URL, score float32) *orchestratorPool {
+	if len(uris) <= 0 {
+		// Should we return here?
+		glog.Error("Orchestrator pool does not have any URIs")
+	}
+	infos := make([]common.OrchestratorLocalInfo, 0, len(uris))
+	for _, uri := range uris {
+		infos = append(infos, common.OrchestratorLocalInfo{URL: uri, Score: score})
+	}
+
+	return &orchestratorPool{infos: infos, bcast: bcast}
+}
+
+func NewOrchestratorPoolWithPred(bcast common.Broadcaster, addresses []*url.URL,
+	pred func(*net.OrchestratorInfo) bool, score float32) *orchestratorPool {
+
+	pool := NewOrchestratorPool(bcast, addresses, score)
+	pool.pred = pred
+	return pool
+}
+
+func (o *orchestratorPool) GetInfos() []common.OrchestratorLocalInfo {
+	return o.infos
+}
+
+func (o *orchestratorPool) GetInfo(uri string) common.OrchestratorLocalInfo {
+	var res common.OrchestratorLocalInfo
+	for _, info := range o.infos {
+		if info.URL.String() == uri {
+			res = info
+			break
+		}
+	}
+	return res
+}
+
+func (o *orchestratorPool) GetOrchestrators(ctx context.Context, numOrchestrators int, suspender common.Suspender, caps common.CapabilityComparator,
+	scorePred common.ScorePred) ([]*net.OrchestratorInfo, error) {
+
+	//linfos := make([]common.OrchestratorLocalInfo, 0, len(o.infos))
+	//for _, info := range o.infos {
+	//	if scorePred(info.Score) {
+	//		linfos = append(linfos, info)
+	//	}
+	//}
+	//
+	//numAvailableOrchs := len(linfos)
+	//numOrchestrators = int(math.Min(float64(numAvailableOrchs), float64(numOrchestrators)))
+
+	// The following allows us to avoid capability check for jobs that only
+	// depend on "legacy" features, since older orchestrators support these
+	// features without capability discovery. This enables interop between
+	// older orchestrators and newer orchestrators as long as the job only
+	// requires the legacy feature set.
+	//
+	// When / if it's justified to completely break interop with older
+	// orchestrators, then we can probably remove this check and work with
+	// the assumption that all orchestrators support capability discovery.
+	legacyCapsOnly := caps.LegacyOnly()
+
+	isCompatible := func(info *net.OrchestratorInfo) bool {
+		if o.pred != nil && !o.pred(info) {
+			return false
+		}
+		// Legacy features already have support on the orchestrator.
+		// Capabilities can be omitted in this case for older orchestrators.
+		// Otherwise, capabilities are required to be present.
+		if info.Capabilities == nil {
+			if legacyCapsOnly {
+				return true
+			}
+			return false
+		}
+		return caps.CompatibleWith(info.Capabilities)
+	}
+
+	o.orchInfoCache.refresh(ctx, numOrchestrators, suspender, o.infos, scorePred, o.bcast)
+	orchInfos := o.orchInfoCache.orchInfos()
+	var infos []*net.OrchestratorInfo
+	for _, info := range orchInfos {
+		if isCompatible(info) {
 			infos = append(infos, info)
 		}
 	}
 
-	clog.Infof(ctx, "Done fetching orch info numOrch=%d responses=%d/%d timedOut=%t",
-		len(infos), nbResp, len(uris), timedOut)
+	clog.Infof(ctx, "Done Getting orchestrators")
 	return infos, nil
+
+	// TODO: Check this part if we need it and if suspension should be per stream
+	//if len(infos) < numOrchestrators {
+	//	diff := numOrchestrators - len(infos)
+	//	for i := 0; i < diff && suspendedInfos.Len() > 0; i++ {
+	//		info := heap.Pop(suspendedInfos).(*suspension).orch
+	//		infos = append(infos, info)
+	//	}
+	//}
+
+	//clog.Infof(ctx, "Done fetching orch info numOrch=%d responses=%d/%d timedOut=%t",
+	//	len(infos), nbResp, len(uris), timedOut)
+	//return infos, nil
 }
 
 func (o *orchestratorPool) Size() int {
